73f8602e2c0a18061c8fa332ccc3b277864a32f6
Sandra-Chaparro <55746359+Sandra-Chaparro@users.noreply.github.com>
2022-03-22 13:14:17 -0500

    Update Database.php
    Moving inside connect

--- config/Database.php
+++ config/Database.php
@@ -1,19 +1,19 @@
 <?php
 
 class Database {
-    //DB Params
-    $url = getenv('JAWSDB_URL');
-    $dbparts = parse_url($url);
-    
-    private $host = $dbparts['host'];
-    private $db_name = ltrim($dbparts['JAWSDB_DB'], '/');
-    private $username = $dbparts['user'];
-    private $password = $dbparts['pass'];
+  
     private $conn;
 
     //DB Connect
     public function connect(){
         $this->conn = null;
+           $url = getenv('JAWSDB_URL');
+            $dbparts = parse_url($url);
+    
+     $host = $dbparts['host'];
+     $db_name = ltrim($dbparts['JAWSDB_DB'], '/');
+     $username = $dbparts['user'];
+     $password = $dbparts['pass'];
 
         try{
             $this->conn = new PDO('mysql:host='. $this->host . ';dbname='. $this->db_name,
