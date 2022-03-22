bff97881a0254ccbe5c7c36a23dc707d76154252
Sandra-Chaparro <55746359+Sandra-Chaparro@users.noreply.github.com>
2022-03-22 12:49:00 -0500

    Update Database.php
    fixing pass

--- config/Database.php
+++ config/Database.php
@@ -2,10 +2,13 @@
 
 class Database {
     //DB Params
+    $url = getenv('JAWSDB_URL');
+    $dbparts = parse_url($url);
+    
     private $host = 'acw2033ndw0at1t7.cbetxkdyhwsb.us-east-1.rds.amazonaws.com';
     private $db_name = 'mrnpq02zo5ekan3h';
     private $username = 'qb9a5as9snhkq974';
-    private $password = getenv('JAWSDB_URL');
+    private $password = $dbparts['pass'];
     private $conn;
 
     //DB Connect
