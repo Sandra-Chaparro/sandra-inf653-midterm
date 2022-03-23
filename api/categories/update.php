a1b6a930cd8933fca85ba55be159ed759908f943
Sandra-Chaparro <55746359+Sandra-Chaparro@users.noreply.github.com>
2022-03-23 15:10:09 -0500

    Update update.php

--- api/categories/update.php
+++ api/categories/update.php
@@ -14,12 +14,18 @@ $category = new Categories($db);
 //Get the raw  data
 $data = json_decode(file_get_contents("php://input"));
 
+if($data->id == null || $data->category == null){
+echo json_encode('message' => 'Missing Required Parameters");
+    die();
+}
+
 $category->id = $data->id;
 $category->category = $data->category;
 
 if($category->update()){
     echo json_encode(
-        array('message' => 'Category updated')
+        array('id' => $category->id,
+        'category' => $category->category)
     );
 } else{
     echo json_encode(
