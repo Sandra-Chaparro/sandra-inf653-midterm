2d714b09698014785f32db06b03aec457d3b5ada
Sandra-Chaparro <55746359+Sandra-Chaparro@users.noreply.github.com>
2022-03-23 17:09:08 -0500

    Update delete.php

--- api/quotes/delete.php
+++ api/quotes/delete.php
@@ -18,19 +18,21 @@ $data = json_decode(file_get_contents("php://input"));
 
 $quote->id = $data->id;
 
-$result = $quote->delete();
-
-if($result){
+if($quote->delete()){
      if($quote->id){
        echo json_encode(
        array('id' => $quote->id)
        );      
      }
-} 
-
+     else{
+          echo json_encode(
+               array('message' => 'No Quotes Found');
+          );
+     }
+}
 
 else{
         echo json_encode(
-        array('message' => 'No Quotes Found')
+        array('message' => 'Fail Delete')
         );
      }  
