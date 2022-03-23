d9afe80ba425dd2572d68b44c85670491ea0d42e
Sandra-Chaparro <55746359+Sandra-Chaparro@users.noreply.github.com>
2022-03-23 09:13:34 -0500

    Update create.php
    include authorId and categoryId

--- api/quotes/create.php
+++ api/quotes/create.php
@@ -14,16 +14,44 @@ $quote = new Quotes($db);
 //Get the raw  data
 $data = json_decode(file_get_contents("php://input"));
 
+if($data->authorId == null || $data->categoryId == null || $data->quote ==null){
+    echo json_encode(
+        array(
+          'message' => 'Missing Required Parameters')    
+    );
+    die();
+}
+
+$category = new Categories();
+$category->id = $data->categoryId;
+$category->read_single();
+
+$author = new Authors();
+$author->id = $data->$authorId;
+$author->read_single();
+
+if($author->id == null){
+    echo json_encode(
+        array('message' => 'authorId not Found')
+    );
+    die();
+}
+
 $quote->quote = $data->quote;
 $quote->authorId = $data->authorId;
 $quote->categoryId = $data->categoryId;
 
 if($quote->create()){
     echo json_encode(
-        array('message' => 'Quote created')
+        array(
+            'id' => $quote->id,
+            'quote' => $quote->quote,
+            'authorId' => $quote->authorId,
+            'categoryId' => $quote->categoryId
+        )
     );
 } else{
     echo json_encode(
-        array('message' => 'Quote NO Created')
+        array('message' => 'Quote Not Created')
     );
 }
