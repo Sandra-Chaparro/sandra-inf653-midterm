00528986f2adf78fcbe1c76a9a9ea2c905930339
Sandra-Chaparro <55746359+Sandra-Chaparro@users.noreply.github.com>
2022-03-23 17:53:10 -0500

    Update update.php

--- api/authors/update.php
+++ api/authors/update.php
@@ -13,16 +13,28 @@ $author = new Authors($db);
 
 //Get the raw  data
 $data = json_decode(file_get_contents("php://input"));
+
+if($data->id ==null || $data->author ==null){
+    echo json_encode(
+        array('message' => Missing Required Parameters)
+    );
+    die();
+}
+
+
 $author->id = $data->id;
 $author->author = $data->author;
 
 if($author->update()){
     echo json_encode(
-        array('message' => 'Post updated')
+        array(
+            'id' => '$author->id',
+            'author' => '$author->author'
+        )
     );
 } else{
     echo json_encode(
-        array('message' => 'Post NO updated')
+            array('message' => 'Author Not updated')
     );
 }
 
