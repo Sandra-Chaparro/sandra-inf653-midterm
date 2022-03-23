add1911d98c4bdce7428789bee3d01903cf2d117
Sandra-Chaparro <55746359+Sandra-Chaparro@users.noreply.github.com>
2022-03-22 19:04:32 -0500

    Update read.php

--- api/categories/read.php
+++ api/categories/read.php
@@ -17,7 +17,6 @@ $num = $result->rowCount();
 
 if($num > 0 ){
   $categories_arr = array();
-  $categories_arr['data'] = array();
 
   while($row = $result->fetch(PDO::FETCH_ASSOC)){
     extract($row);
@@ -27,7 +26,7 @@ if($num > 0 ){
       'category' => $category
     );
 
-    array_push($categories_arr['data'], $category_item);
+    array_push($categories_arr, $category_item);
 
   }
 
@@ -37,7 +36,7 @@ if($num > 0 ){
 }
 else{
   echo json_encode(
-    array('message' => 'No categories found')
+    array('message' => 'No Categories Found')
   );
 }
 
