a775c78f6f3c6c6d245cb24a071ec7d4cefe6f5b
Sandra-Chaparro <55746359+Sandra-Chaparro@users.noreply.github.com>
2022-03-23 22:48:07 -0500

    Update create.php

--- api/categories/create.php
+++ api/categories/create.php
@@ -14,14 +14,24 @@ $category = new Categories($db);
 //Get the raw  data
 $data = json_decode(file_get_contents("php://input"));
 
+if($data->category == null){
+    echo json_encode(
+        array('message' => 'Missing Required Parameters')
+    );
+    die();
+}
+
 $category->category = $data->category;
 
 if($category->create()){
     echo json_encode(
-        array('message' => 'Category created')
+        array(
+            'id' => $category->id,
+            'category' => $category->$category
+        )
     );
 } else{
     echo json_encode(
-        array('message' => 'Category NO Created')
+        array('message' => 'Category Not Created')
     );
 }
